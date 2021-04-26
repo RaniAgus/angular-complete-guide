77d3a0959c85300d320ab10038f91bbe6987e639
Agustin Ranieri <agustinra@shifta.la>
2021-04-26 12:07:33 -0300

    017 - creando un nuevo componente

---
+++ bootstrap-app/src/app/server/server.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({ // Component decorator
+    selector: 'app-server', // The HTML tag 'app-x'
+    templateUrl: './server.component.html' // El path (relativo) hacia el template HTML
+})
+export class ServerComponent {
+
+}
