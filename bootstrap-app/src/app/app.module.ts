cb918b66ae14958056930e165a449bc937b2e942
Agustin Ranieri <agustinra@shifta.la>
2021-04-26 12:19:12 -0300

    020 - gemero el componente 'servers' con ng

--- bootstrap-app/src/app/app.module.ts
+++ bootstrap-app/src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component'; // 5 - el cual se importa acá
-import { ServerComponent } from './server/server.component' // (018) 2 - el cual se importa acá
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component' // (018) 2 - el cual se importa acá
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent // (018) 1 - Agregamos el componente 'server'
+    ServerComponent,
+    ServersComponent // (018) 1 - Agregamos el componente 'server'
   ],
   imports: [
     BrowserModule
