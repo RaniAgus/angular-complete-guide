0958c0d1a8b419f4870d0530f88a9a6c6af5d738
Agustin Ranieri <agustinra@shifta.la>
2021-04-27 10:44:27 -0300

    040 - ngClass

--- bootstrap-app/src/app/server/server.component.ts
+++ bootstrap-app/src/app/server/server.component.ts
@@ -2,7 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({ // Component decorator
     selector: 'app-server', // The HTML tag 'app-x'
-    templateUrl: './server.component.html' // El path (relativo) hacia el template HTML
+    templateUrl: './server.component.html', // El path (relativo) hacia el template HTML
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
 })
 export class ServerComponent {
     serverId:number = 12;
