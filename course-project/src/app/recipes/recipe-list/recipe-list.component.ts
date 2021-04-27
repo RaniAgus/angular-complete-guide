b455f99c92fd91677c3a8696c9f847f84c3b1fcb
Agustin Ranieri <agustinra@shifta.la>
2021-04-27 14:59:34 -0300

    053 - agrego contenido a la lista de recetas

--- course-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes:Recipe[] = [
+    {
+      name: 'A test recipe',
+      description: 'This is simply a test',
+      imagePath: 'https://via.placeholder.com/150'
+    }
+  ];
 
   constructor() { }
 
