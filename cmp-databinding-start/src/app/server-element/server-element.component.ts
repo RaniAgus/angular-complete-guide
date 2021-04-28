69f5f28977b80b8db0a23eeda9947c8000cb2521
Agustin Ranieri <agustinra@shifta.la>
2021-04-28 15:19:59 -0300

    068 - Defino un alias para la propiedad

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
-  @Input() element: {type: string, name: string, content: string};
+  @Input('srvElement') // Para definirlo como alias para esta propiedad desde fuera
+  element: {type: string, name: string, content: string};
 
   constructor() { }
 
