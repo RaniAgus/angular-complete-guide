ab781bb7d4b876869b6ff91c283bd83bdd891353
Agustin Ranieri <agustinra@shifta.la>
2021-04-28 15:16:34 -0300

    067 - Binding custom property via Input decorator

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = 
+    [
+      { type: 'server'
+      , name: 'Test server'
+      , content: 'Just a test!'
+      }
+    ]
+    ;
 }
