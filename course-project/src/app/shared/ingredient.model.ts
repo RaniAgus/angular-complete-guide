2f596dd5677f5ec3753fdc5052d7edfed628d5b1
Agustin Ranieri <agustinra@shifta.la>
2021-04-28 09:38:21 -0300

    057 - Creo el modelo ingrediente

---
+++ course-project/src/app/shared/ingredient.model.ts
@@ -0,0 +1,9 @@
+export class Ingredient {
+    //public name: string;
+    //public amount: number;
+
+    constructor(public name: string, public amount: number) { // Esto nos provee las propiedades automáticamente
+        //this.name = name;
+        //this.amount = amount;
+    }
+}
