a9333fbb30df79b60f4c6c821fba219f679c0888
Agustin Ranieri <agustinra@shifta.la>
2021-04-29 10:21:38 -0300

    079 - ngAfterContent: Init y Checked

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck { // Es una buena práctica incluir la interfaz si la estás implementando
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked { // Es una buena práctica incluir la interfaz si la estás implementando
   @Input('srvElement') // Para definirlo como alias para esta propiedad desde fuera
   element: {type: string, name: string, content: string};
 
@@ -27,5 +27,13 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck { // E
 
   ngDoCheck() {
     console.log('doCheck called!'); // No es un problema siempre y cuando el código que se ejecuta es chiquito
+  }
+
+  ngAfterContentInit() {
+    console.log('contentInit called!'); // Después de onInit y de doCheck, solo una vez
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!'); // Siempre después de doCheck
   }
 }
