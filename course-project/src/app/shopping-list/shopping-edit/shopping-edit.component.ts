ea3e197aca21d45eae0058954f5436b9b6fca395
Agustin Ranieri <agustinra@shifta.la>
2021-04-29 15:15:57 -0300

    090 - add ingredients to shopping list

--- course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ course-project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { Ingredient } from './../../shared/ingredient.model'
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  @ViewChild('nameInput', {static: true})
+  nameInputRef: ElementRef;
+
+  @ViewChild('amountInput', {static: true})
+  amountInputRef: ElementRef;
+
+  @Output()
+  addedIngredient = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAdd() {
+    this.addedIngredient.emit
+      ( new Ingredient
+        ( this.nameInputRef.nativeElement.value
+        , this.amountInputRef.nativeElement.value
+        )
+      )
+    ;
+
   }
 
 }
--- course-project/src/app/shopping-list/shopping-list.component.ts
+++ course-project/src/app/shopping-list/shopping-list.component.ts
@@ -20,4 +20,8 @@ export class ShoppingListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onAddition(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
+
 }
