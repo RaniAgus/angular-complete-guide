93f2a38f7b20e8e991499090cc326e9dab34218a
Agustin Ranieri <agustinra@shifta.la>
2021-04-29 10:27:51 -0300

    079 - AfterView: Init y Content

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,11 +1,28 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked } from '@angular/core';
+import { Component
+       , OnInit
+       , Input
+       , OnChanges
+       , SimpleChanges
+       , DoCheck
+       , AfterContentInit
+       , AfterContentChecked
+       , AfterViewInit
+       , AfterViewChecked 
+       } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked { // Es una buena práctica incluir la interfaz si la estás implementando
+export class ServerElementComponent implements OnInit // Es una buena práctica incluir la interfaz si la estás implementando
+                                             , OnChanges
+                                             , DoCheck
+                                             , AfterContentInit
+                                             , AfterContentChecked
+                                             , AfterViewInit
+                                             , AfterViewChecked
+  {
   @Input('srvElement') // Para definirlo como alias para esta propiedad desde fuera
   element: {type: string, name: string, content: string};
 
@@ -30,10 +47,18 @@ export class ServerElementComponent implements OnInit, OnChanges, DoCheck, After
   }
 
   ngAfterContentInit() {
-    console.log('contentInit called!'); // Después de onInit y de doCheck, solo una vez
+    console.log('ngAfterContentInit called!'); // Después de onInit y de doCheck, solo una vez
   }
 
   ngAfterContentChecked() {
     console.log('ngAfterContentChecked called!'); // Siempre después de doCheck
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!'); // Después de ContentInit
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!'); // Después de ContentCheck
   }
 }
