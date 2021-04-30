9cc713e9748647a1cd3047a7661e7a1daf0f394f
Agustin Ranieri <agustinra@shifta.la>
2021-04-30 10:02:39 -0300

    094 - basic highlight attribute directive

---
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, OnInit } from "@angular/core";
+
+@Directive
+(
+    { selector: '[appBasicHighlight]' //Los corchetes son un agregado
+    }
+)
+export class BasicHighlightDirective implements OnInit {
+    constructor(private elementRef: ElementRef) { 
+        // Se agrega la referencia en el constructor
+        // Es ElementRef, como @ViewChild
+    }
+
+    ngOnInit() {
+        // Se le puede setear, por ejemplo, un estilo
+        this.elementRef.nativeElement.style.backgroundColor = 'lightblue';
+    }
+}
