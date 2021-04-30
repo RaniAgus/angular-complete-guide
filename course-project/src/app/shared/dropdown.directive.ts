9e547de929cc401c7022943a09695db1641d4eb0
Agustin Ranieri <agustinra@shifta.la>
2021-04-30 12:03:43 -0300

    104 - cerrar el dropdown desde fuera

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -1,18 +1,21 @@
-import { Directive, HostBinding, HostListener, OnInit } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener, OnInit } from "@angular/core";
 
 @Directive({
     selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit{
-    @HostBinding('class.open') isOpen:boolean;
+    @HostBinding('class.open') isOpen:boolean = false;
 
-    constructor() { }
+    constructor(private elementRef: ElementRef) { } // Recibimos la referencia al elemento
 
-    ngOnInit() {
-        this.isOpen = false;
-    }
+    ngOnInit() { }
 
-    @HostListener('click') toggleOpen(eventdata: Event) {
-        this.isOpen = !this.isOpen;
+    // Recibimos el evento click desde cualquier parte del documento
+    @HostListener('document:click', ['$event']) toggleOpen(eventdata: Event) {
+        this.isOpen = this.elementRef
+            .nativeElement               // Si el elemento HTML que tiene esta directiva
+            .contains(eventdata.target)  // contiene al elemento al que se hizo click,
+            ? !this.isOpen               // se abre o cierra dependiendo de si ya estaba abierto.
+            : false;                     // Sino, se hizo click afuera, entonces siempre se cierra.
     }
 }
