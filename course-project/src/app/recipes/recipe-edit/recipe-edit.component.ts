94ca9be45c26f04e9f7a747f765c18680a18776b
Agustin Ranieri <agustinra@shifta.la>
2021-05-10 12:54:50 -0300

    227 - creo el formulario

--- course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ course-project/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,18 +11,50 @@ import { ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-  id: number;
   editMode: boolean = false;
 
-  constructor(private route: ActivatedRoute) { }
+  recipe: Recipe;
+  recipeForm: FormGroup;
+
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(
       (params: Params) => {
-        this.id = +params['id'];
-        this.editMode = params['id'] != null;
+        if(params['id'] != null) {
+          this.editMode = true;
+          this.recipe = this.recipeService.getRecipeById(+params['id']);
+          this.initForm(); // Muy importante cargarlo aca para cuando se reinicie la pagina!
+        }
       }
     );
   }
 
+  get ingredients() {
+    return (this.recipeForm.get('ingredients') as FormArray).controls;
+  }
+
+  onSubmit() {
+    console.log(this.recipeForm);
+  }
+
+  private initForm() {
+    this.recipeForm = new FormGroup
+    ( { 'name': new FormControl(this.editMode ? this.recipe.name : '')
+      , 'description': new FormControl(this.editMode ? this.recipe.description : '')
+      , 'imagePath': new FormControl(this.editMode ? this.recipe.imagePath : '')
+      , 'ingredients': new FormArray
+          ( this.editMode 
+              ? this.recipe.ingredients.map
+                  ( (ingredient: Ingredient) => new FormGroup
+                    ( { 'name': new FormControl(ingredient.name)
+                      , 'amount': new FormControl(ingredient.amount)
+                      }
+                    )
+                  ) 
+              : []
+          )
+      }
+    );
+  }
 }
--- course-project/src/app/recipes/recipe.model.ts
+++ course-project/src/app/recipes/recipe.model.ts
@@ -1,5 +1,10 @@
 import { Ingredient } from "../shared/ingredient.model";
 
 export class Recipe {
-    constructor(public id:number, public name: string, public description: string, public imagePath: string, public ingredients: Ingredient[]) {}
+    constructor
+        ( public id:number
+        , public name: string
+        , public description: string
+        , public imagePath: string
+        , public ingredients: Ingredient[] ) {}
 }
