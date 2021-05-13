7b2179b69aa8a7e93b7c0502b18b8e5b6e823095
Agustin Ranieri <agustinra@shifta.la>
2021-05-13 11:14:57 -0300

    273 - modificar http requests en el interceptor

--- http-start/src/app/auth-interceptor.service.ts
+++ http-start/src/app/auth-interceptor.service.ts
@@ -5,6 +5,8 @@ export class AuthInterceptorService implements HttpInterceptor{
     // Este código se ejecuta por cada request que se haga en toda la app
     intercept(request: HttpRequest<any>, next: HttpHandler) {
         console.log('Request is on its way');
-        return next.handle(request); // Esto permite que la solicitud continúe
+        // Las requests son inmutables, hay que crear una nueva y pasarle qué atributos queremos sobreescribir
+        const modifiedRequest = request.clone({headers: request.headers.append('Auth', 'xyz')}); 
+        return next.handle(modifiedRequest); // Esto permite que la solicitud continúe
     }
 }
