2632d6025584dd4a3104c6c47ab50b7bde0afa41
Agustin Ranieri <agustinra@shifta.la>
2021-05-13 11:32:39 -0300

    275 - interceptores multiples

--- http-start/src/app/auth-interceptor.service.ts
+++ http-start/src/app/auth-interceptor.service.ts
@@ -1,10 +1,10 @@
 import { HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
 
     // Este código se ejecuta por cada request que se haga en toda la app
     intercept(request: HttpRequest<any>, next: HttpHandler) {
-        console.log('Request is on its way');
         // Las requests son inmutables, hay que crear una nueva y pasarle qué atributos queremos sobreescribir
         const modifiedRequest = request.clone({headers: request.headers.append('Auth', 'xyz')}); 
         return next.handle(modifiedRequest); // Esto permite que la solicitud continúe
