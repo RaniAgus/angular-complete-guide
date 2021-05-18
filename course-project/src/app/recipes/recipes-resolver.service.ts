a88e59b22358338a0c748385695a49425b909fb8
Agustin Ranieri <agustinra@shifta.la>
2021-05-18 11:51:27 -0300

    285 - fix al editar recetas

--- course-project/src/app/recipes/recipes-resolver.service.ts
+++ course-project/src/app/recipes/recipes-resolver.service.ts
@@ -2,15 +2,17 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.dataStorageService.fetchRecipes();
+    const recipes = this.recipeService.getRecipes();
+    return recipes.length === 0 ? this.dataStorageService.fetchRecipes() : recipes;
   }
 }
