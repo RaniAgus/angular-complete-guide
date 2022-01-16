a37a418e2d519386d21fe8107eb01dbeb32933ea
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-16 16:18:33 -0300

    329 - creo un auth module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -1,41 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 
 import { HttpClientModule } from '@angular/common/http';
-import { AuthComponent } from './auth/auth.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule
 (
   { declarations: 
       [ AppComponent
       , HeaderComponent
-      , AuthComponent
       ]
   , imports: 
-      [ // ngIf y ngFor vienen de acá
-        BrowserModule 
-        // las features de forms vienen de acá
-      , FormsModule
-      , ReactiveFormsModule
-        // esto solo provee servicios (no directivas ni componentes), por lo que
-        // va a estar disponible en toda la app
-      , HttpClientModule 
-      , AppRoutingModule
+      [ AppRoutingModule
+      , AuthModule
       , RecipesModule
       , ShoppingListModule
       , SharedModule
       , CoreModule
+      // ngIf y ngFor vienen de acá, junto con cosas de inicialización de la app
+      , BrowserModule 
+      // esto solo provee servicios (no directivas ni componentes), por lo que
+      // va a estar disponible en toda la app
+      , HttpClientModule 
       ]
   , bootstrap: [AppComponent]
   }
 )
-export class AppModule { }
+export class AppModule {}
