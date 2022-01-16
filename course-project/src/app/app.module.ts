01574831d5dfbf5a39b3672f6a6ba7a7bbb830a1
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-16 16:00:56 -0300

    328 - agrego core module

--- course-project/src/app/app.module.ts
+++ course-project/src/app/app.module.ts
@@ -2,29 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatStepperModule } from '@angular/material/stepper'
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 
-import { RecipeService } from './recipes/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { MatDialogModule } from '@angular/material/dialog';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule
 (
   { declarations: 
       [ AppComponent
       , HeaderComponent
-      , NotFoundComponent
       , AuthComponent
       ]
   , imports: 
@@ -40,14 +33,7 @@ import { SharedModule } from './shared/shared.module';
       , RecipesModule
       , ShoppingListModule
       , SharedModule
-      ]
-  , providers: 
-      [ RecipeService
-      , ShoppingListService
-      , { provide: HTTP_INTERCEPTORS
-        , useClass: AuthInterceptorService
-        , multi: true
-        }
+      , CoreModule
       ]
   , bootstrap: [AppComponent]
   }
