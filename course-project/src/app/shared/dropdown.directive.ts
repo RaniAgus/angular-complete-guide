d16bf4c5d99a48246c976041345fdb326ac89b56
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-16 03:45:54 -0300

    detalles de dropdownDirective

--- course-project/src/app/shared/dropdown.directive.ts
+++ course-project/src/app/shared/dropdown.directive.ts
@@ -4,18 +4,16 @@ import { Directive, ElementRef, HostBinding, HostListener, OnInit } from "@angul
     selector: '[appDropdown]'
 })
 export class DropdownDirective implements OnInit{
-    @HostBinding('class.open') isOpen:boolean = false;
+    @HostBinding('class.open')
+    isOpen: boolean = false;
 
     constructor(private elementRef: ElementRef) { } // Recibimos la referencia al elemento
 
     ngOnInit() { }
 
     // Recibimos el evento click desde cualquier parte del documento
-    @HostListener('document:click', ['$event']) toggleOpen(eventdata: Event) {
-        this.isOpen = this.elementRef
-            .nativeElement               // Si el elemento HTML que tiene esta directiva
-            .contains(eventdata.target)  // contiene al elemento al que se hizo click,
-            ? !this.isOpen               // se abre o cierra dependiendo de si ya estaba abierto.
-            : false;                     // Sino, se hizo click afuera, entonces siempre se cierra.
+    @HostListener('click', ['$event']) 
+    toggleOpen($event: Event) {
+        this.isOpen = !this.isOpen;
     }
 }
