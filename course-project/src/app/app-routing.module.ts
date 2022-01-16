726bc9a9c68d8f66dfd4b63ce22b5f32cc3e610e
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-16 16:01:19 -0300

    quito componente en desuso

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from "./auth/auth.component";
-import { NotFoundComponent } from "./not-found/not-found.component";
 
 const appRoutes: Routes = 
   [ { path: '', redirectTo: '/recipes', pathMatch: 'full' }
   , { path: 'auth'         , component: AuthComponent     }
-  , { path: '404'          , component: NotFoundComponent }
 // Tuve que quitarlo para que no me mande acá al entrar a /recipes
 //, { path: '**'           , redirectTo: '/404'           }
   ]
