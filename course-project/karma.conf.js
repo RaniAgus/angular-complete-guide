43c83c83d57a2a12008f2da72284a11bfee76ccd
Agustin Ranieri <aranieri@hexacta.com>
2022-08-19 09:56:05 -0300

    agrego code coverage

--- course-project/karma.conf.js
+++ course-project/karma.conf.js
@@ -53,6 +53,22 @@ module.exports = function (config) {
       }
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/course-project'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' },
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
+    }
   });
 };
