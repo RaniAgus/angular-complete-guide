31799944ee153cbc7c6f6eed19047b769225e01d
Agustin Ranieri <aranieri@hexacta.com>
2022-08-19 10:00:32 -0300

    angular testing modules

--- course-project/src/app/header/header.component.spec.ts
+++ course-project/src/app/header/header.component.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
@@ -8,7 +10,8 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      imports: [ HttpClientTestingModule, RouterTestingModule ],
+      declarations: [ HeaderComponent ],
     })
     .compileComponents();
   });
